3be12989e7f24eba128496e36f1af6d60bcc6bf7
Umar22222 <asadovumar222@gmail.com>
2024-08-04 04:03:38 +0500

    homework

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.here__wrapper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
